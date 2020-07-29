c8fb5fbd416cd4fe0226377826dc6886022a6b63
Roberto Febrer <febrer@gmail.com>
2020-07-29 07:50:12 +0200

    add signalR project chat

---
+++ SignalR/ChatHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using SignalR.Models;
+
+namespace SignalR
+{
+    public class ChatHub: Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "Chat Room", "Hi There! What Can I help you with?", DateTime.Now);
+            await base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string name, string text)
+        {
+            var message = new ChatMessage
+            {
+                Message = text,
+                User = name,
+                Sent = DateTimeOffset.UtcNow
+            };
+            await Clients.All.SendAsync("ReceiveMessage",message.User,message.Message, message.Sent) ;
+
+        }
+    }
+}
---
+++ SignalR/Models/ChatMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SignalR.Models
+{
+    public class ChatMessage
+    {
+        public string Message { get; set; }
+        public string User { get; set; }
+        public DateTimeOffset Sent { get; set; }
+    }
+}
